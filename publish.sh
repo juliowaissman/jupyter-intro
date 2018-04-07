84678a96341da1e166d5fd4dc1799e20122e847e
juliowaissman <juliowaissman@gmail.com>
2018-04-07 09:36:41 -0700

    Actualiza

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-jupyter-nbconvert --to slides jupyter_basico.ipynb --reveal-prefix=reveal.js
+# jupyter-nbconvert --to slides jupyter_basico.ipynb --reveal-prefix=reveal.js
+jupyter-nbconvert --to slides jupyter_basico.ipynb --reveal-prefix="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.1.0"
 mv jupyter_basico.slides.html index.html
 git add -A .
 git commit -m "Actualiza"
