8935e01c4e647064a717af684f51042247890f69
Julio Waissman Vilanova <juliowaissman@gmail.com>
2018-04-06 21:57:14 -0700

    Actualiza

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 jupyter-nbconvert --to slides jupyter_basico.ipynb --reveal-prefix=reveal.js
+mv jupyter_basico.slides.html index.html
 git add -A .
 git commit -m "Actualiza"
 git push origin master
