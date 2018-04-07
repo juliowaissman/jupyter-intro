c5065aec51cbd4a26dd274f69ed0a900946d80a7
Julio Waissman Vilanova <juliowaissman@gmail.com>
2018-04-06 21:55:19 -0700

    Actualiza

--- publish.sh
+++ publish.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 jupyter-nbconvert --to slides jupyter_basico.ipynb --reveal-prefix=reveal.js
-mv jupyter_basico.slides.html  index.html
-mkdir -p /tmp/workspace
-cp -r * /tmp/workspace/
 git add -A .
 git commit -m "Actualiza"
-git checkout -B gh-pages
-cp -r /tmp/workspace/* .
-git add -A .
-git commit -m "nueva version"
-git push origin master gh-pages
-git checkout master
-rm -rf /tmp/workspace
+git push origin master
+
